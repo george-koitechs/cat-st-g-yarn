f00a856537e60645d0899027bece99bebc687713
george-koitechs <george.koitechs@gmail.com>
2023-02-14 19:03:41 +0200

    work on pet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import type { HeadFC } from 'gatsby';
+import type { HeadFC, PageProps } from 'gatsby';
 
 import { Layout } from '../domains/layout/layout.component';
 import { productsService } from '../domains/products/products.service';
@@ -7,24 +7,29 @@ import { Product } from 'swell-js';
 import { HomePage } from '../domains/home/home.page';
 import { IPageProps } from '../__common/types';
 
-// const IndexPage: React.FC<IPageProps<{ products: Product[] }>> = (props) => {
-const IndexPage = () => {
+const IndexPage: React.FC<IPageProps<{ data: any }>> = (props) => {
+  // const IndexPage = (props:PageProps) => {
+  console.log('data', props.serverData);
   return (
     <Layout>
-      <h1>adadsa</h1>
+      <h1>{props.serverData.data?.title}</h1>
+      <h1>asdasd</h1>
       {/*<HomePage products={props.serverData.products} />*/}
     </Layout>
   );
 };
 
-// export async function getServerData() {
-//   // const productsData = await productsService.getAll();
-//
-//   return {
-//     props: {},
-//     // props: { products: productsData.results },
-//   };
-// }
+export async function getServerData() {
+  // const productsData = await productsService.getAll();
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => json);
+
+  return {
+    props: { data },
+    // props: { products: productsData.results },
+  };
+}
 
 export default IndexPage;
 
